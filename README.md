448b069eeb448eee172b4a8e8854fc31112852ca
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2022-03-14 17:16:02 -0400

    Monday update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Book-Library
 Book Library API
+Simple API using Flask
+
+Created a 3 book database using a Dictionary in Python, then I used the following methods:
+1) GET METHOD to collect the information from my computer into the web browser
+2) POST METHOD to execute 2 things:
+    a. to add a 4th book to the database
+    b. to update the information on book #3 inside the database (I could have used the PUT METHOD as well, however, Pascal said that for this, the POST METHOD is easier)
+3) DELETE METHOD to delete a specified book from the dictionary
--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def hello():
 def book_list():
     return books_db
 
-@app.route("/books_rating", methods = ['PUT']) #Trying to update my book list dictionary by adding a master rating to the list
+@app.route("/books_rating", methods=['PUT']) #Trying to update my book list dictionary by adding a master rating to the list
 def books_rating ():
     return "From 1 to 5, how would you rate this list?" 
     if request.methods == 'PUT' :
@@ -28,15 +28,39 @@ def books_rating ():
     else :
             return books_db.append("This is a highly rated book list")
 
-@app.route("/new_book", methods = ['POST']) #trying to add a book to my book list
-def new_book_request():
-    return "Would you like to add your favorite book to this list? Type yes or no "
-    if request :
-        "yes"
-        return def new_book():
-                {"4": {"name": "abc", "release_date": "1112"}}
-                return books_db.append(new_book)
+@app.route("/book/new_book", methods=['POST']) #trying to add a book to my book list
+def add_book():
+    # Collect the new book from the user/url
+    request_data = request.get_json() # Use the 'request' function on python to REQUEST DATA from the user
+   
+    # Extract the movie data from the request
+    new_book = request_data['book']
+    # Get the last position in the database
+    new_id = len(books_db) + 1
+    # create a new new entry for my movie
+    new_book_data = { str(new_id) : new_book }
+    # Update 
+    books_db.update(new_book_data)
+    return "This book was added successfully"
+
+
+ # Using the POST METHOD to update an element in the list, for example the <release_date>. I can also use the PUT METHOD to do this   
+@app.route("/book/update_book_information", methods=['POST'])
+def update_book_info():
+    request_data = request.get_json()
+    books_db.update(request_data)
+    return "This book was updated"
+
+@app.route("/books/delete_books/<book_id>")
+def delete_books(book_id):
+    del books_db[book_id]
+    return "This book was removed"
+   
+    
+@app.route("/book/<book_id>") #This is a GET METHOD. The GET Method is just to collect something
+def get_book(book_id):
+    return books_db[book_id]
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0") # The Host will be the IP address of my own machine
+    app.run(host="127.0.0.1") # The Host will be the IP address of my own machine
